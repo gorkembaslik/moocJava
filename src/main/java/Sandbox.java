c1b86d15a7cdd41a13311e9fe9646de9f66cf1fa
Gorkem Baslik <gorkem20011@gmail.com>
2024-04-20 15:40:47 +0200

    added Salvo to the first..

--- src/main/java/Sandbox.java
+++ src/main/java/Sandbox.java
@@ -3,6 +3,6 @@ public class Sandbox {
 
     public static void main(String[] args) {
         // Write your program here
-        System.out.println("Welcome to programming");
+        System.out.println("Welcome to programming Salvo!");
     }
 }
